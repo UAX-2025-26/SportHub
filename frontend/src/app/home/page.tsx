33adbf40c7d8b6782d7d30fb7f9fed705ed6b5bc
rdk <rradu@myuax.com>
2025-12-02 11:52:57 +0100

    backup

---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,44 @@
+import React, { FunctionComponent } from 'react';
+import MainDownComponent from "@/components/desarrollo/MainDownComponent";
+import clsx from "clsx";
+import bodyStyles from "@/components/main-layout/body/MainBody.module.css";
+import MainHeader from "@/components/main-layout/header/MainHeader";
+import headerStyles from "@/components/main-layout/header/MainHeader.module.css";
+import MainContent from "@/components/main-layout/content/MainContent";
+import contentStyles from "@/components/main-layout/content/MainContent.module.css";
+import Imagen from "@/components/desarrollo/Imagen";
+import MainBody from "@/components/main-layout/body/MainBody";
+import RoundedButton from "@/components/common/button/profile-button/RoundedButton";
+
+
+const HomePage: FunctionComponent = () => {
+    return (
+        <MainBody bodyClassName={clsx(bodyStyles.content)}>
+            <MainHeader bodyClassName={clsx(headerStyles.container, headerStyles.title, bodyStyles.header)} />
+            <MainContent bodyClassName={clsx(contentStyles.container, bodyStyles.body)}>
+                <div className={clsx(contentStyles.flexContainer)}>
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                    <Imagen />
+                </div>
+            </MainContent>
+            <div className={clsx(bodyStyles.footer)}>
+            <MainDownComponent>
+                <h1>Elige tu deporte</h1>
+            </MainDownComponent>
+            </div>
+
+        </MainBody>
+    );
+};
+
+export default HomePage;
--- frontend/src/components/main-layout/header/MainHeader.tsx
+++ frontend/src/components/main-layout/header/MainHeader.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from 'react'
+import clsx from "clsx";
 
-interface HomepageBodyProps {
+
+interface Props {
     bodyClassName?: string; // Class for the body container
     titleClassName?: string; // Class for the title
     title?: string; // Title text, default to "SPORTHUB."
     children?: React.ReactNode; // Content to render inside the body
 }
 
-const HomepageBody: React.FC<HomepageBodyProps> = ({
-    bodyClassName,
-    titleClassName,
-    title = "SPORTHUB", // Default title
-    children
-}) => {
+const Mainpage: React.FC<Props> = ({ bodyClassName, titleClassName, title='SPORTHUB', children }) => {
     return (
         <div className={clsx(bodyClassName)}>
             <h1 className={clsx(titleClassName)}>{title}</h1>
             {children}
         </div>
     );
-};
+}
 
-export default React.memo(HomepageBody);
+export default React.memo(Mainpage);
